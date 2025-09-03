17e0238236d08bd931756472782f16121cad3ba1
kayque-1 <kayqueantonio2007@gmail.com>
2025-09-03 20:37:06 -0300

    Update Tubarao.java

--- Tubarao.java
+++ Tubarao.java
@@ -1,6 +1,6 @@
 public class Tubarao extends Animal {
     @Override
     public void emitirSom() {
-        System.out.println("O tubarão morde: crunch!");
+        System.out.println("O tubarão emite alta frequencia: tiro!");
     }
 }
