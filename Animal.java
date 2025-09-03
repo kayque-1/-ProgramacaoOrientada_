6a1264e7bba4355d35698b6405672a466b9ba656
kayque-1 <kayqueantonio2007@gmail.com>
2025-09-03 20:35:38 -0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    public String nome;
+    public String raca;
+    public Double peso;
+
+    public void emitirSom() {
+        System.out.println("O animal faz um som.");
+    }
+}
